5f6eaafcab64da6ecba52b14881c3aca24b03bf1
Margarita Berseneva <rita7798@gmail.com>
2017-10-06 22:30:35 +0300

    gazeta-bam.ru ver_2.1

--- homework/02.corpora/выкачивание интернета.py
+++ homework/02.corpora/выкачивание интернета.py
@@ -20,20 +20,23 @@ REGSCRIPT = re.compile('<script>.*?</script>', flags=re.U | re.DOTALL)
 REGSCRIPT1 = re.compile('<style>.*?</style>', flags=re.U | re.DOTALL) 
 REGSCRIPT2 = re.compile('<script type.*?>.*?</script>', flags=re.U | re.DOTALL)
 REGSCRIPT3 = re.compile('<style type.*?>.*?</style>', flags=re.U | re.DOTALL) 
-REGCOMMENT = re.compile('<!--.*?-->', flags=re.U | re.DOTALL) 
+REGCOMMENT = re.compile('<!--.*?-->', flags=re.U | re.DOTALL)
 
 
 def download_page():
-        for k in range (1,763): # переход по страницам с новостями (12 штук на каждой странице) всего -- 763 (3.10.17)
+        for k in range (1,765): # переход по страницам с новостями (12 штук на каждой странице)
                 textpage = ""
                 number = str(k + 1)
-                page = urllib.request.urlopen(PAGEURL + number)
-                print("{} страница обрабатывается".format(k))
-                time.sleep(2)
-                textpage = page.read().decode('utf-8')
-                download_news(textpage)
-                print("_______________________")
-
+                try:
+                        page = urllib.request.urlopen(PAGEURL + number)
+                        print("{} страница обрабатывается".format(k))
+                        time.sleep(2)
+                        textpage = page.read().decode('utf-8')
+                        download_news(textpage)
+                        print("_______________________")
+                except:
+                        print("Error at the {}".format(page))
+                
 
 def download_news(textpage):
         news_set = set()
@@ -68,24 +71,23 @@ def information(textnews, fullurl):
         day = REGDAY.search(textnews).group(1)
         month = REGMONTH.search(textnews).group(1)
         year = REGYEAR.search(textnews).group(1)
-        data = "{}.{}.{}".format(day, month, year)
-        metadata(author, name, data, category, fullurl, year, month) # запись метадаты
+        date = "{}.{}.{}".format(day, month, year)
+        metadata(author, name, date, category, fullurl, year, month) # запись метадаты
         clean_t = cleaning(textnews)
         onewordname = name.replace(" ", "_")
         onewordname = re.sub("[.,!?()\"-\/]", "", onewordname )
-        making_plain(clean_t, author, onewordname, month, year, data, category, fullurl, name) # запись специального plain текста
+        making_plain(clean_t, author, onewordname, month, year, date, category, fullurl, name) # запись специального plain текста
         tagged_xml(month, year, onewordname, name) # запись tagged xml текста
         tagged_plain(month, year, onewordname, name) # запись tagged plain текста
-        making_plain1(clean_t, author, onewordname, month, year, data, category, fullurl, name) # запись plain текста
-        print("page done.")
+        making_plain1(clean_t, author, onewordname, month, year, date, category, fullurl, name) # запись plain текста
+        print("новость готова.")
 
         
-def metadata(author, name, data, category, fullurl, year, month):
-        path = "БАМ{}plain{}{}{}{}".format(os.sep, os.sep, year, os.sep, month)
-        row = '\n{}\t{}\t\t\t{}\t{}\tпублицистика\t\t\t{}\t\tнейтральный\tн-возраст\tн-уровень\tрайонная\t{}\tБАМ\t\t{}\tгазета\tРоссия\tАмурская область\tru'.format(path, author, name, data, category, fullurl, year)
+def metadata(author, name, date, category, fullurl, year, month):
+        path = "БАМ{}plain{}{}{}{}{}{}.txt".format(os.sep, os.sep, year, os.sep, month, os.sep, name)
         with open (os.path.join('БАМ', 'metadata.csv'), 'a', encoding = 'utf-8') as f:
                 output = csv.writer(f, delimiter='\t')
-                output.writerow(["{}\t{}\t\t\t{}\t{}\tпублицистика\t\t\t{}\t\tнейтральный\tн-возраст\tн-уровень\tрайонная\t{}\tБАМ\t\t{}\tгазета\tРоссия\tАмурская область\tru".format(path, author, name, data, category, fullurl, year)])
+                output.writerow(["\n{}\t{}\t\t\t{}\t{}\tпублицистика\t\t\t{}\t\tнейтральный\tн-возраст\tн-уровень\tрайонная\t{}\tБАМ\t\t{}\tгазета\tРоссия\tАмурская область\tru".format(path, html.unescape(author), html.unescape(name), date, category, fullurl, year)])
         print("метаданные записались.")
         return
 
@@ -100,7 +102,7 @@ def cleaning(textnews):
         return clean_t
 
 
-def making_plain(clean_t, author, onewordname, month, year, data, category, fullurl, name):
+def making_plain(clean_t, author, onewordname, month, year, date, category, fullurl, name):
         path = "БАМ{}plain{}{}{}{}".format(os.sep, os.sep, year, os.sep, month)
         folder = os.path.join("{}{}{}.txt".format(path, os.sep, onewordname))
         if not os.path.exists(path):
@@ -111,8 +113,8 @@ def making_plain(clean_t, author, onewordname, month, year, data, category, full
         return
 
 
-def making_plain1(clean_t, author, onewordname, month, year, data, category, fullurl, name):
-        plain = "@au {}\n@ti {}\n@da {}\n@topic {}\n@url {}\n{}".format(author, name, data, category, fullurl, clean_t)
+def making_plain1(clean_t, author, onewordname, month, year, date, category, fullurl, name):
+        plain = "@au {}\n@ti {}\n@da {}\n@topic {}\n@url {}\n{}".format(author, name, date, category, fullurl, clean_t)
         path = "БАМ{}plain{}{}{}{}" .format(os.sep,  os.sep, year, os.sep, month)
         folder = os.path.join(path, onewordname + ".txt")
         if not os.path.exists(path):
@@ -124,29 +126,29 @@ def making_plain1(clean_t, author, onewordname, month, year, data, category, ful
 
 
 def tagged_xml(month, year, onewordname, name):
-        path_i = "/Users/margaritaberseneva/Desktop/БАМ/plain/{}{}{}{}".format(year, os.sep, month, os.sep)
-        path_o = "/Users/margaritaberseneva/Desktop/БАМ/mystem-xml/{}{}{}{}".format(year, os.sep, month, os.sep)
+        path_i = "{}Users{}margaritaberseneva{}Desktop{}БАМ{}plain{}{}{}{}{}".format(os.sep, os.sep, os.sep, os.sep, os.sep, os.sep, year, os.sep, month, os.sep)
+        path_o = "{}Users{}margaritaberseneva{}Desktop{}БАМ{}mystem-xml{}{}{}{}{}".format(os.sep, os.sep, os.sep, os.sep, os.sep, os.sep, year, os.sep, month, os.sep)
         if not os.path.exists(path_o):
                 os.makedirs(path_o)
-        os.system(("/Users/margaritaberseneva/Downloads/mystem -idn --format xml {}{}.txt {}{}.xml").format(path_i, onewordname, path_o, onewordname))
+        os.system(("{}Users{}margaritaberseneva{}Downloads{}mystem -idn --format xml {}{}.txt {}{}.xml").format(os.sep, os.sep, os.sep, os.sep, path_i, onewordname, path_o, onewordname))
         print("размеченный xml текст готов.")
 
 
 def tagged_plain(month, year, onewordname, name):
-        path_i = "/Users/margaritaberseneva/Desktop/БАМ/plain/{}{}{}{}".format(year, os.sep, month, os.sep)
-        path_o = "/Users/margaritaberseneva/Desktop/БАМ/mystem-plain/{}{}{}{}".format(year, os.sep, month, os.sep)
+        path_i = "{}Users{}margaritaberseneva{}Desktop{}БАМ{}plain{}{}{}{}{}".format(os.sep, os.sep, os.sep, os.sep, os.sep, os.sep, year, os.sep, month, os.sep)
+        path_o = "{}Users{}margaritaberseneva{}Desktop{}БАМ{}mystem-plain{}{}{}{}{}".format(os.sep, os.sep, os.sep, os.sep, os.sep, os.sep, year, os.sep, month, os.sep)
         if not os.path.exists(path_o):
                 os.makedirs(path_o)
-        os.system(("/Users/margaritaberseneva/Downloads/mystem -idn {}{}.txt {}{}.txt").format(path_i, onewordname, path_o, onewordname))
+        os.system(("{}Users{}margaritaberseneva{}Downloads{}mystem -idn {}{}.txt {}{}.txt").format(os.sep, os.sep, os.sep, os.sep, path_i, onewordname, path_o, onewordname))
         print("размеченный plain текст готов.")
 
 
 def main():
-        if not os.path.exists("/Users/margaritaberseneva/Desktop/БАМ"):
+        if not os.path.exists("{}Users{}margaritaberseneva{}Desktop{}БАМ".format(os.sep, os.sep, os.sep, os.sep)):
                 os.mkdir("БАМ")
         with open (os.path.join('БАМ', 'metadata.csv'), 'a', encoding = 'utf-8') as f:
                 output = csv.writer(f, delimiter='\t')
-                header = ['path' + '\t' + 'author' + '\t' + 'sex' + '\t' + 'birthday' + '\t' + 'header' + '\t' + 'created' + '\t' + 'sphere' + '\t' + 'genre_fi' + '\t' + 'type' + '\t' + 'topic' + '\t' + 'chronotop' + '\t' + 'style' + '\t' + 'audience_age' + '\t' + 'audience_level' + '\t' + 'audience_size' + '\t' + 'source' + '\t' + 'publication' + '\t' + 'publ_year' + '\t' + 'medium' + '\t' + 'country' + '\t' + 'region' + '\t' + 'language' + '\n']
+                header = ['path' + '\t' + 'author' + '\t' + 'sex' + '\t' + 'birthday' + '\t' + 'header' + '\t' + 'created' + '\t' + 'sphere' + '\t' + 'genre_fi' + '\t' + 'type' + '\t' + 'topic' + '\t' + 'chronotop' + '\t' + 'style' + '\t' + 'audience_age' + '\t' + 'audience_level' + '\t' + 'audience_size' + '\t' + 'source' + '\t' + 'publication' + '\t' + 'publ_year' + '\t' + 'medium' + '\t' + 'country' + '\t' + 'region' + '\t' + 'language' ]
                 output.writerow(header)
         download_page()
 
