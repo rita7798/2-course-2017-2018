4343cc2cebcd24e388f2ef07d1be3d795d90bf57
Margarita Berseneva <rita7798@gmail.com>
2017-12-17 14:17:37 +0300

    matplotlib ver_2.0

--- homework/05.SQLite/1.py
+++ homework/05.SQLite/1.py
@@ -1,4 +1,6 @@
 import sqlite3
+import matplotlib.pyplot as plt
+
 
 def db_1():
     conn = sqlite3.connect("hittite.db")
@@ -9,8 +11,9 @@ def db_1():
     d = {}
     j = 1
     for arr in t:
-        for i in  arr:
-            d[j] = [arr[0], arr[1], arr[2]]
+        for i in arr:
+            g = arr[2].replace(".", " ")
+            d[j] = [arr[0], arr[1], g]
             j += 1
             break
     conn = sqlite3.connect("words.sqlite")
@@ -45,14 +48,22 @@ def db_2():
 
 
 def db_3(d):
+    a = ["C", "N", "P", "Q", "V"] 
     for k,v in d.items():
         conn = sqlite3.connect("words.sqlite")
         c4 = conn.cursor()
-        c4.execute("""SELECT id
-                             FROM words
-                             WHERE glosses
-                             LIKE \"{}\"""".format(k))
-        t = c4.fetchall()
+        if k not in a:
+            c4.execute("""SELECT id
+                                 FROM words
+                                 WHERE glosses
+                                 LIKE \"%{}%\"""".format(k))
+            t = c4.fetchall()
+        else:
+            c4.execute("""SELECT id
+                                 FROM words
+                                 WHERE glosses
+                                 LIKE \"% {} %\"""".format(k))
+            t = c4.fetchall()
         if t != []:
             conn = sqlite3.connect("glosses.sqlite")
             c5 = conn.cursor()
@@ -75,10 +86,219 @@ def db_3(d):
                         conn.commit()
 
 
+def values():
+    d = {}
+    conn = sqlite3.connect("glosses.sqlite")
+    c6 = conn.cursor()
+    c6.execute("""SELECT id
+                         FROM glosses""")
+    m = c6.fetchall()
+    graph_1(m)
+    graph_2(m)
+    graph_3(m)
+    graph_4(m)
+    graph_5(m)
+    graph_6(m)
+
+
+def graph_1(m):
+    x1 = []
+    y1 = []
+    for arr in m:
+        for i in arr:
+            if i<= 21:
+                conn = sqlite3.connect("w-to-g.sqlite")
+                c7 = conn.cursor()
+                c7.execute("""SELECT id_gloss
+                                     FROM  words_to_glosses
+                                     WHERE id_gloss = \"{}\" """.format(i))
+                n = c7.fetchall()
+                if (len(n)) != 0:
+                    y1.append((len(n)))
+                    conn = sqlite3.connect("glosses.sqlite")
+                    c8 = conn.cursor()
+                    c8.execute("""SELECT name
+                                         FROM glosses
+                                         WHERE id = \"{}\" """.format(i))
+                    p = c8.fetchone()
+                    for k in p:
+                        x1.append(k)
+            else:
+                    continue
+    plt.rc('xtick', labelsize=7) 
+    plt.bar(x1, y1)
+    plt.title("1")
+    plt.xlabel("gloss")
+    plt.ylabel("quantity")
+    plt.savefig('plot_1.png')
+    plt.figure()
+
+
+def graph_2(m):
+    x2 = []
+    y2 = []
+    for arr in m:
+        for i in arr:
+            if 22<=i<=24:
+                conn = sqlite3.connect("w-to-g.sqlite")
+                c10 = conn.cursor()
+                c10.execute("""SELECT id_gloss
+                                     FROM  words_to_glosses
+                                     WHERE id_gloss = \"{}\" """.format(i))
+                n = c10.fetchall()
+                if (len(n)) != 0:
+                    y2.append((len(n)))
+                    conn = sqlite3.connect("glosses.sqlite")
+                    c11 = conn.cursor()
+                    c11.execute("""SELECT name
+                                         FROM glosses
+                                         WHERE id = \"{}\" """.format(i))
+                    p = c11.fetchone()
+                    for k in p:
+                        x2.append(k)
+            else:
+                    continue
+    plt.bar(x2, y2)
+    plt.title("2")
+    plt.xlabel("gloss")
+    plt.ylabel("quantity")
+    plt.savefig('plot_2.png')
+    plt.figure()
+
+
+def graph_3(m):
+    x3 = []
+    y3 = []
+    for arr in m:
+        for i in arr:
+            if 25<=i<=31:
+                conn = sqlite3.connect("w-to-g.sqlite")
+                c7 = conn.cursor()
+                c7.execute("""SELECT id_gloss
+                                     FROM  words_to_glosses
+                                     WHERE id_gloss = \"{}\" """.format(i))
+                n = c7.fetchall()
+                if (len(n)) != 0:
+                    y3.append((len(n)))
+                    conn = sqlite3.connect("glosses.sqlite")
+                    c8 = conn.cursor()
+                    c8.execute("""SELECT name
+                                         FROM glosses
+                                         WHERE id = \"{}\" """.format(i))
+                    p = c8.fetchone()
+                    for k in p:
+                        x3.append(k)
+            else:
+                    continue
+    plt.bar(x3, y3)
+    plt.title("3")
+    plt.xlabel("gloss")
+    plt.ylabel("quantity")
+    plt.savefig('plot_3.png')
+    plt.figure()
+
+
+def graph_4(m):
+    x4 = []
+    y4 = []
+    for arr in m:
+        for i in arr:
+            if 32<=i<=40:
+                conn = sqlite3.connect("w-to-g.sqlite")
+                c7 = conn.cursor()
+                c7.execute("""SELECT id_gloss
+                                     FROM  words_to_glosses
+                                     WHERE id_gloss = \"{}\" """.format(i))
+                n = c7.fetchall()
+                if (len(n)) != 0:
+                    y4.append((len(n)))
+                    conn = sqlite3.connect("glosses.sqlite")
+                    c8 = conn.cursor()
+                    c8.execute("""SELECT name
+                                         FROM glosses
+                                         WHERE id = \"{}\" """.format(i))
+                    p = c8.fetchone()
+                    for k in p:
+                        x4.append(k)
+            else:
+                    continue
+    plt.bar(x4, y4)
+    plt.title("4")
+    plt.xlabel("gloss")
+    plt.ylabel("quantity")
+    plt.savefig('plot_4.png')
+    plt.figure()
+
+
+def graph_5(m):
+    x5 = []
+    y5 = []
+    for arr in m:
+        for i in arr:
+            if 41<=i<=42:
+                conn = sqlite3.connect("w-to-g.sqlite")
+                c7 = conn.cursor()
+                c7.execute("""SELECT id_gloss
+                                     FROM  words_to_glosses
+                                     WHERE id_gloss = \"{}\" """.format(i))
+                n = c7.fetchall()
+                if (len(n)) != 0:
+                    y5.append((len(n)))
+                    conn = sqlite3.connect("glosses.sqlite")
+                    c8 = conn.cursor()
+                    c8.execute("""SELECT name
+                                         FROM glosses
+                                         WHERE id = \"{}\" """.format(i))
+                    p = c8.fetchone()
+                    for k in p:
+                        x5.append(k)
+            else:
+                    continue
+    plt.bar(x5, y5)
+    plt.title("5")
+    plt.xlabel("gloss")
+    plt.ylabel("quantity")
+    plt.savefig('plot_5.png')
+    plt.figure()
+
+
+def graph_6(m):
+    x6 = []
+    y6 = []
+    for arr in m:
+        for i in arr:
+            if 43<=i<=48:
+                conn = sqlite3.connect("w-to-g.sqlite")
+                c7 = conn.cursor()
+                c7.execute("""SELECT id_gloss
+                                     FROM  words_to_glosses
+                                     WHERE id_gloss = \"{}\" """.format(i))
+                n = c7.fetchall()
+                if (len(n)) != 0:
+                    y6.append((len(n)))
+                    conn = sqlite3.connect("glosses.sqlite")
+                    c8 = conn.cursor()
+                    c8.execute("""SELECT name
+                                         FROM glosses
+                                         WHERE id = \"{}\" """.format(i))
+                    p = c8.fetchone()
+                    for k in p:
+                        x6.append(k)
+            else:
+                    continue
+    plt.bar(x6, y6)
+    plt.title("6")
+    plt.xlabel("gloss")
+    plt.ylabel("quantity")
+    plt.savefig('plot_6.png')
+    plt.figure()
+
+
 def main():
     db_1()
     d = db_2()
     db_3(d)
+    values()
 
     
 if __name__ == "__main__":
